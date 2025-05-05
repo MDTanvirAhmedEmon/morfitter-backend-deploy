ab235f9e66f89098a0483bed6635ed259e4cecdb
MDTanvirAhmedEmon <mdtanvirahmedemon17@gmail.com>
2025-05-05 15:16:05 +0600

    update url

--- src/app/helpers/fileUploader.ts
+++ src/app/helpers/fileUploader.ts
@@ -2,30 +2,6 @@ import { v2 as cloudinary } from 'cloudinary'
 import multer from 'multer'
 import * as fs from 'fs'
 
-// cloudinary.config({
-//   cloud_name: 'dck3z93x5',
-//   api_key: '958848183433213',
-//   api_secret: 'hoZys5CcfirZpIrYgTQihz7fI6M',
-// })
-
-
-// export const uploadToCloudinary = async (file: any) => {
-//   return new Promise((resolve, reject) => {
-//     cloudinary.uploader.upload(
-//       file.path,
-//       { public_id: file.originalname },
-//       function (error, result) {
-//         fs.unlinkSync(file.path)
-//         if (error) {
-//           reject(error)
-//         } else {
-//           resolve(result)
-//         }
-//       },
-//     )
-//   })
-// }
-
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(
