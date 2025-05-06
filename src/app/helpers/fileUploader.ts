b24ee8148b9c23d575d38ea51e714d1abe9852d2
MDTanvirAhmedEmon <mdtanvirahmedemon17@gmail.com>
2025-05-06 14:22:41 +0600

    made file name unique

--- src/app/helpers/fileUploader.ts
+++ src/app/helpers/fileUploader.ts
@@ -1,18 +1,20 @@
-import { v2 as cloudinary } from 'cloudinary'
-import multer from 'multer'
-import * as fs from 'fs'
+import { v2 as cloudinary } from 'cloudinary';
+import multer from 'multer';
+import * as fs from 'fs';
+import { v4 as uuidv4 } from 'uuid';
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(
       null,
-      '/server/morfitter-backend-deploy/uploads'
-      // '/web-development/Back End/shiloh-morfitter-backend/uploads',
-    )
+      // '/server/morfitter-backend-deploy/uploads'
+      '/web-development/Back End/shiloh-morfitter-backend/uploads',
+    );
   },
   filename: function (req, file, cb) {
-    cb(null, file.originalname)
+    const uniqueName = `${uuidv4()}-${file.originalname}`;
+    cb(null, uniqueName); // Save file with the unique name
   },
-})
+});
 
-export const upload = multer({ storage: storage })
+export const upload = multer({ storage: storage });
